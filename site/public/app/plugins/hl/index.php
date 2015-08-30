ff98ca07cfa0d9da99d288388d2fefa9a4b0dc2b
Luke Lanchester <luke@lukelanchester.com>
2015-08-30 16:26:41 +0100

    Test WordPress composer install

---
+++ site/public/app/plugins/hl/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: HybridLogic
+Plugin URI: http://hybridlogic.co.uk
+Description: The hybridlogic app.
+Version: 8.0.0
+Author: Luke Lanchester
+Author URI: http://hybridlogic.co.uk
+Created: 2015-08-29
+Updated: 2015-08-29
+Repo URI: github.com/Dachande663/hybridlogic
+*/
+
+
+// Define
+define('HL_CORE_INIT', true);
+define('HL_CORE_DIR', plugin_dir_path(__FILE__));
+define('HL_CORE_URL', plugin_dir_url(__FILE__));
---
+++ site/public/app/themes/rullington/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>HybridLogic &middot; Luke Lanchester, Web Developer &amp; Geek</title>
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Montserrat|Enriqueta">
+	<link rel="stylesheet" type="text/css" href="/app/themes/rullington/assets/css/site.css">
+</head>
+
+<body>
+
+
+<div id="header">
+	<div class="container">
+		<h1><a href="/" alt="Homepage"><span>HYBRID</span>logic</a></h1>
+		<ul id="menu">
+			<li><a href="/">Home</a></li>
+			<li><a href="/content">Content</a></li>
+			<li><a href="/about">About</a></li>
+		</ul>
+	</div>
+</div>
+
+
+
+
+<div class="container cols">
+
+	<div class="col col--9 text--primary">
+		<h1><?php the_title(); ?></h1>
+	</div>
+
+</div>
+
+
+
+
+
+
+<div class="container" id="footer">
+	<p>Copyright &copy; Luke Lanchester 2005 - 2015</p>
+	<p class="right">So long and thanks for all the fish</p>
+</div>
+
+
+
+</body>
+</html>
